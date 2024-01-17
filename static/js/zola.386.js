d488f4906980e0e218f64c6fa5d28d31eef35f4a
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-01-17 17:48:38 +0500

    time to shine my friends

--- static/js/zola.386.js
+++ static/js/zola.386.js
@@ -3,3 +3,14 @@ _386 = {
   onePass: true,
   speedFactor: 3
 };
+
+document.addEventListener('DOMContentLoaded', function() {
+  var rainbowText = document.querySelector('.brand');
+  var text = rainbowText.innerText;
+  rainbowText.innerText = '';
+  for (var i = 0; i < text.length; i++) {
+    var charElem = document.createElement('span');
+    charElem.innerText = text[i];
+    rainbowText.appendChild(charElem);
+  }
+});
